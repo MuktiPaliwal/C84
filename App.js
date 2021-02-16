270803908f20c7c1b78977cbdd16a8ca5414e09a
MuktiPaliwal <65341949+MuktiPaliwal@users.noreply.github.com>
2021-02-16 18:20:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import { AppTabNavigator } from './components/AppTabNavigator'
+import {AppDrawerNavigator} from './components/AppDrawerNavigator'
+import {AppStackNavigator} from './components/AppStackNavigator'
+
+export default function App() {
+  return (
+    <AppContainer/>
+  );
+}
+
+
+const switchNavigator = createSwitchNavigator({
+  WelcomeScreen:{screen: WelcomeScreen},
+  Drawer:{screen: AppDrawerNavigator},
+  Stack:{screen: AppStackNavigator}
+})
+
+const AppContainer =  createAppContainer(switchNavigator);
---
+++ screens/MyDonationScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, TextInput, Alert } from 'react-native';
+import {Card} from 'react-native-elements'
+
+export default class MyDonationScreen extends Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    My Donations Screen
+                </Text>
+            </View>
+        )
+    }
+}
